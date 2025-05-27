1c9dd072e720cdc049707b7ac732551968fe96b3
tomer gur <tomer.gur@amwell.com>
2025-05-27 21:13:05 +0300

    Update Stage D logic to handle trait combinations (TP, AE, AP, TE) with ranking system

--- app/utils/pdn_calculator.py
+++ app/utils/pdn_calculator.py
@@ -76,7 +76,7 @@ def calculate_pdn_code(answers: dict) -> dict:
 
 
     # Stage C: Validation and Tie-Breaking
-    for i in range(52, 58):
+    for i in range(41, 46):
         if str(i) in answers:
             ranking = answers[str(i)]['ranking']
             traits = list(ranking.keys())
@@ -105,24 +105,35 @@ def calculate_pdn_code(answers: dict) -> dict:
     logger.info("Stage C: Trait Calculation for E %s", result['scores']['E'])
     logger.info("Stage C dominant trait %s", dominant_trait)
 
-
-# StageD: Strengthen Dominant Trait
-    trait_counts = {'A': 0, 'T': 0, 'P': 0, 'E': 0}
-    for i in range(58, 61):
+    # Stage D: Validation and Tie-Breaking
+    for i in range(46, 60):
         if str(i) in answers:
             ranking = answers[str(i)]['ranking']
-            for trait, rank in ranking.items():
-                if rank == 1:
-                    trait_counts[trait] += 8  
-                elif rank == 2:
-                    trait_counts[trait] += 6
-                elif rank == 3:
-                    trait_counts[trait] += 4
-                elif rank == 4:
-                    trait_counts[trait] += 2
+            # Get the trait combinations and their rankings
+            trait_combinations = list(ranking.keys())
+            if len(trait_combinations) == 2:
+                combo1, combo2 = trait_combinations
+                value1, value2 = ranking[combo1], ranking[combo2]
 
-    for trait, score in trait_counts.items():
-        result['scores'][trait] += score
+                difference = value1 - value2
+                score_adjustment = abs(difference) * 2
+
+                if difference > 0:
+                    # Add points to both traits in the winning combination
+                    result['scores'][combo1[0]] += score_adjustment
+                    result['scores'][combo1[1]] += score_adjustment
+                    # Subtract points from both traits in the losing combination
+                    result['scores'][combo2[0]] -= score_adjustment
+                    result['scores'][combo2[1]] -= score_adjustment
+                elif difference < 0:
+                    # Add points to both traits in the winning combination
+                    result['scores'][combo2[0]] += score_adjustment
+                    result['scores'][combo2[1]] += score_adjustment
+                    # Subtract points from both traits in the losing combination
+                    result['scores'][combo1[0]] -= score_adjustment
+                    result['scores'][combo1[1]] -= score_adjustment
+
+    # Recalculate dominant trait after all adjustments
     dominant_trait = max(result['scores'], key=result['scores'].get)
     result['trait'] = dominant_trait
 
@@ -131,6 +142,30 @@ def calculate_pdn_code(answers: dict) -> dict:
     logger.info("Stage D: Trait Calculation for P %s", result['scores']['P'])
     logger.info("Stage D: Trait Calculation for E %s", result['scores']['E'])
     logger.info("Stage D dominant trait %s", dominant_trait)
+
+    # StageE: Strengthen Dominant Trait
+    trait_counts = {'A': 0, 'T': 0, 'P': 0, 'E': 0}
+    for i in range(60, 63):
+        if str(i) in answers:
+            ranking = answers[str(i)]['ranking']
+            for trait, rank in ranking.items():
+                if rank == 1:
+                    result['scores'][trait] += 8  
+                elif rank == 2:
+                    result['scores'][trait] += 6
+                elif rank == 3:
+                    result['scores'][trait] += 4
+                elif rank == 4:
+                    result['scores'][trait] += 2
+
+    dominant_trait = max(result['scores'], key=result['scores'].get)
+    result['trait'] = dominant_trait
+
+    logger.info("Stage E: Trait Calculation for A %s", result['scores']['A'])
+    logger.info("Stage E: Trait Calculation for T %s", result['scores']['T'])
+    logger.info("Stage E: Trait Calculation for P %s", result['scores']['P'])
+    logger.info("Stage E: Trait Calculation for E %s", result['scores']['E'])
+    logger.info("Stage E dominant trait %s", dominant_trait)
 
 
     # Finalizing the PDN code
